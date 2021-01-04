e56715e1a5059c014017d2150bf34dcdec205b0a
Jonas Broms <jonas.broms@gmail.com>
2021-01-04 16:39:42 +0200

    fix: Normalize CSS & remove 8px margin on body

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routing } from './routes/Routing'
 import { Navbar } from './components/organisms/navbar/'
 import { Provider } from './shared/Provider'
+import './app.css'
 
 function App() {
   return (
