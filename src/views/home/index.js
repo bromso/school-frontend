72e785210cf303fae97e03deda975780d794550f
Jonas Broms <jonas.broms@gmail.com>
2021-01-06 19:39:05 +0200

    test: test external api response

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Axios from 'axios'
 
 export const Home = () => {
+
+	const fetchDataFromExternalAPI = () => {
+		Axios.get("https://pokeapi.co/api/v2/pokemon/ditto")
+			.then((response) => console.log(response))
+			.catch((error) => console.log(error))
+	}
+
 	return (
 		<>
 			<h1>Home view</h1>
+			<button onClick={() => fetchDataFromExternalAPI()}>Make API call</button>
 		</>
 	)
 }
