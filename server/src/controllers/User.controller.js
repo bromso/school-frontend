6d41f2a4f069ce5b588e8ac9f0defece6cd25737
Jonas Broms <jonas.broms@gmail.com>
2021-01-07 20:59:43 +0200

    feat: create a get all users controller & routes

--- server/src/controllers/User.controller.js
+++ server/src/controllers/User.controller.js
@@ -14,6 +14,17 @@ const createUser = async (req, res) => {
 	}
 }
 
+const getAllUsers = async (req, res) => {
+	try {
+		const response = await UserModel.find()
+		res.status(200).send(response)
+	} catch(error) {
+		res.status(500).send({ message: error.message })
+	}
+
+}
+
 export default {
-	createUser
+	createUser,
+	getAllUsers
 }
