177d69a5faaaec208f2b8dcf86967ffbd8db4401
Jonas Broms <jonas.broms@gmail.com>
2021-01-07 11:57:21 +0200

    feat: add mongoose

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ import express from 'express'
 import dotenv from 'dotenv'
 import helmet from 'helmet'
 import morgan from 'morgan'
+import mongoose from 'mongoose'
 import middlewares from './src/middlewares/index.js'
+
 
 dotenv.config()
 const app = express()
@@ -16,6 +18,14 @@ app.get('/recipe', (req, res) => {
 
 app.use(middlewares.notFound)
 app.use(middlewares.errorHandler)
+
+mongoose.connect('mongod://localhost/recipedb', {useNewUrlParser: true, useUnifiedTopology: true})
+	.then(() => console.log('Successfully connected to the database'))
+	.catch((error) => {
+		console.log('ERROR WHILE TRYING TO CONNECT TO THE DATABASE: ', error)
+		process.exit()
+	})
+
 
 app.listen(port, () => {
 	console.log(`Server is working and available on port ${port}`)
